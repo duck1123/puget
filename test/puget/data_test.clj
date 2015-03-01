8a8c8776bc309365d2642a49047f915426f2cf8d
Greg Look <greg@greg-look.net>
2015-03-01 11:53:11 -0800

    Drop edn-str tests and uri bin extensions.

--- test/puget/data_test.clj
+++ test/puget/data_test.clj
@@ -6,13 +6,10 @@
 
 
 (defn test-tagged-literal
-  [data t f & [reader]]
+  [data t f]
   (let [{:keys [tag form]} (data/->edn data)]
     (is (= t tag))
-    (is (= f form)))
-  (let [s (data/edn-str data)
-        data' (edn/read-string (if reader {:readers {t reader}} {}) s)]
-    (is (= data data'))))
+    (is (= f form))))
 
 
 (deftest inst-tagged-literals
@@ -27,37 +24,16 @@
     'uuid "96d91316-53b9-4800-81c1-97ae9f4b86b0"))
 
 
-(deftest uri-tagged-literals
-  (test-tagged-literal
-    (java.net.URI. "http://en.wikipedia.org/wiki/Uniform_resource_identifier")
-    'puget/uri "http://en.wikipedia.org/wiki/Uniform_resource_identifier"
-    data/read-uri))
-
-
-(deftest bin-tagged-literals
-  (let [byte-arr (.getBytes "foobarbaz")
-        {:keys [tag form]} (data/->edn byte-arr)
-        string (data/edn-str byte-arr)
-        read-arr (edn/read-string {:readers {'puget/bin data/read-bin}} string)]
-    (is (= 'puget/bin tag))
-    (is (= "Zm9vYmFyYmF6" form))
-    (is (= (count byte-arr) (count read-arr)))
-    (is (= (seq byte-arr) (seq read-arr)))))
-
-
 (deftest generic-tagged-literal
-  (let [data (data/tagged-literal 'foo :bar)
-        {:keys [tag form]} (data/->edn data)
-        string (data/edn-str data)]
-    (is (data/tagged-literal? data))
+  (let [data (tagged-literal 'foo :bar)
+        {:keys [tag form]} (data/->edn data)]
     (is (= 'foo tag))
     (is (= :bar form))
-    (is (= "#foo :bar" string (str data)))
-    (is (= data (edn/read-string {:default data/tagged-literal} string)))))
+    (is (= "#foo :bar" string (str data)))))
 
 
 (defrecord TestRecord [x y])
-(data/extend-tagged-map TestRecord 'test/record)
+(data/extend-map-notation TestRecord 'test/record)
 (deftest tagged-literal-extension
   (let [rec (TestRecord. :foo :bar)
         {:keys [tag form]} (data/->edn rec)]
